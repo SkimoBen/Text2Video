d8b9d4d06dfebeddeeea8cecafcf326afbb32fd4
SkimoBen <pearmanbenjamin@gmail.com>
2023-07-15 12:36:07 -0600

    Initial Commit

---
+++ Text2Video/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Text2Video
+//
+//  Created by Ben Pearman on 2023-07-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Text2Video/Text2VideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text2VideoApp.swift
+//  Text2Video
+//
+//  Created by Ben Pearman on 2023-07-15.
+//
+
+import SwiftUI
+
+@main
+struct Text2VideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
